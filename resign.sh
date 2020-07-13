7109ad50bacb0a34baef18d27efd0ef1542fd34e
elon <540197337@qq.com>
2020-07-13 15:25:48 +0800

    feat: resolve Entitlement,resign.sh
    todo: Add search local project for CFBundleIdentifier and corresponding identity

--- resign.sh
+++ resign.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 TARGET_APP_PATH=$1
 ENTITLEMENTS="Entitlements.plist"
-# todo : add auto Entitlements
-cat <<EOF > ./Entitlements.plist
 
+# todo: Add search local project for CFBundleIdentifier and corresponding identity
+security cms -D -i $TARGET_APP_PATH/embedded.mobileprovision > ./tmp_mobileprovision.txt
+cat <<EOF > ./Entitlements.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
 EOF
+sed -n "`grep -n "Entitlements" ./tmp_mobileprovision.txt|cut -d ':' -f1`,`grep -n '</dict>' ./tmp_mobileprovision.txt|xargs |cut -d ':' -f1`p" ./tmp_mobileprovision.txt >> ./Entitlements.plist
+cat <<EOF >> ./Entitlements.plist
+</plist>
+EOF
+rm ./tmp_mobileprovision.txt
+
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.ss.iphone.ugc.Awemeelon" "$TARGET_APP_PATH/Info.plist"
+
 #----------------------------------------
 
 # 5. 给MachO文件上执行权限
@@ -28,3 +39,4 @@ done
 fi
 
 /usr/bin/codesign -fs "$EXPANDED_CODE_SIGN_IDENTITY" --no-strict --entitlements=$ENTITLEMENTS $TARGET_APP_PATH
+rm $ENTITLEMENTS
